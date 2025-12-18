dfcf40060f7786de7c07eb1c69fdaaad56326fa6
C1R9A7W2D <DrSpore2@gmail.com>
2025-12-18 12:51:52 +0300

    Add CarAI script and update parking spot setup
    Introduced a new CarAI script and attached it to the car prefab. Updated parking spot prefab to use the 'Parking spot' tag and reorganized/added parking spot instances in the SampleScene with adjusted positions and names. Also made minor adjustments to the car prefab's scale.

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -1,19 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
     [SerializeField]
     GameObject carPrefab;
+    private const float PARKING_FILLING = 0.75f;
+    private Quaternion SPAWN_ROTATION = Quaternion.Euler(0, 0, 90);
+    private List<Vector3> carsPositions = new List<Vector3>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(carPrefab, new Vector2(0, UnityEngine.Random.Range(-11, 9)), Quaternion.identity);
+        Vector3[] parkingSpotsPositions = GetParkingSpotsPositions();
+        SpawnCarsInParkingSpots(parkingSpotsPositions);
     }
 
-    // Update is called once per frame
-    void Update()
+    private static Vector3[] GetParkingSpotsPositions()
     {
-        
+        GameObject[] parkingSpots = GameObject.FindGameObjectsWithTag("Parking spot");
+        Vector3[] parkingSpotPositions = new Vector3[parkingSpots.Length];
+
+        for (int i = 0; i < parkingSpots.Length; i++)
+            parkingSpotPositions[i] = parkingSpots[i].transform.position;
+
+        return parkingSpotPositions;
+    }
+
+    private void SpawnCarsInParkingSpots(Vector3[] parkingSpotsPositions)
+    {
+        System.Random random = new();
+
+        for(int i = 0; i < parkingSpotsPositions.Length; i++)
+        {
+            if (random.NextDouble() < PARKING_FILLING)
+            {
+                Instantiate(carPrefab, parkingSpotsPositions[i], SPAWN_ROTATION);
+                carsPositions.Add(parkingSpotsPositions[i]);
+            }
+        }
+    }
+
+    public List<Vector3> GetCarsPositions()
+    {
+        return carsPositions;
     }
 }
