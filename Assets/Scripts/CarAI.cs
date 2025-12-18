9e66121956f913f1953b68f5a4c07cc3d49f4aac
C1R9A7W2D <DrSpore2@gmail.com>
2025-12-18 15:33:47 +0300

    Add AI car prefab and enhance CarAI script
    Добавил отдельный префаб для машины, управляемой ИИ. У этой машины есть простой скрипт, который позволит ей считывать входные данные. Также добавил ей функции ChangeSpeed и Rotate для дальнейшего использования

--- Assets/Scripts/CarAI.cs
+++ Assets/Scripts/CarAI.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class CarAI : MonoBehaviour
 {
+    [SerializeField]
+    float rotation = 0;
+    [SerializeField]
+    Vector2 position = new Vector2(-10, 0.5f);
+    [SerializeField]
+    float sensorRange = 3f;
+    [SerializeField]
+    float maxSpeed = 3f;
+    [SerializeField]
+    bool movesForward = true;
+
+    private float speed = 1f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.SetPositionAndRotation(position, Quaternion.Euler(0, 0, rotation));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Quaternion rot = Quaternion.Euler(0, 0, rotation);
+        Vector3 moveDirection = (rot * Vector3.up).normalized;
+        transform.localPosition += moveDirection * speed * Time.deltaTime;
+    }
+
+    void ChangeSpeed(float delta)
+    {
+        speed += delta;
+    }
+
+    void Rotate(float degrees)
+    {
+        rotation += degrees;
+        transform.Rotate(0, 0, rotation);
     }
 }
